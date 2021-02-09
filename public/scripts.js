2ffb2b3eca7a483062321c0ce40951d81142dcb9
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-09 20:39:13 -0300

    refactor: scripts for detail page

--- public/scripts.js
+++ public/scripts.js
@@ -1,35 +1,44 @@
-const modalOverlay = document.querySelector(".modal__overlay");
 const recipes = document.querySelectorAll(".recipe");
 
-for (let recipe of recipes) {
+for (let i = 0; i < recipes.length; i++) {
+  const recipe = recipes[i];
+
   recipe.addEventListener("click", () => {
-    const elementId = handleId(recipe);
-    const recipeTitle = recipe.querySelector("h3").innerText;
-    const recipeAuthor = recipe.querySelector("p").innerText;
-
-    modalOverlay.classList.add("active");
-
-    modalOverlay.querySelector("img").src = `./assets/${elementId}.png`;
-    modalOverlay.querySelector("h2").innerText = recipeTitle;
-    modalOverlay.querySelector("p").innerText = recipeAuthor;
+    window.location.href = `/recipes/${i}`;
   });
 }
 
-document.querySelector(".modal__close").addEventListener("click", () => {
-  modalOverlay.classList.remove("active");
+const ingredients = document.querySelector(".ingredients ul");
+const preparation = document.querySelector(".preparation ul");
+const information = document.querySelector(".information p");
 
-  modalOverlay.querySelector("img").src = "";
-  modalOverlay.querySelector("h2").innerText = "";
-  modalOverlay.querySelector("p").innerText = "";
-});
+const ingredientsAccordion = document.querySelector("#ingredientsAccordion");
+const preparationAccordion = document.querySelector("#preparationAccordion");
+const informationAccordion = document.querySelector("#informationAccordion");
 
-function handleId(recipe) {
-  const elementId = recipe.getAttribute("id");
-  const formatedId = elementId.split("_");
+const AR_D = "arrow_drop_down";
+const AR_U = "arrow_drop_up";
 
-  if (formatedId.length === 2) {
-    return formatedId[1];
-  }
-  
-  return elementId;
+function handleArrow(element, item) {
+  element.classList.toggle("hidden");
+
+  item.innerHTML === AR_D ? item.innerHTML = AR_U : item.innerHTML = AR_D;
+}
+
+if (ingredients !== null && ingredientsAccordion !== null) {
+  ingredientsAccordion.addEventListener("click", () => {
+    handleArrow(ingredients, ingredientsAccordion);
+  });
+}
+
+if (preparation !== null && preparationAccordion !== null) {
+  preparationAccordion.addEventListener("click", () => {
+    handleArrow(preparation, preparationAccordion);
+  });
+}
+
+if (information !== null && informationAccordion !== null) {
+  informationAccordion.addEventListener("click", () => {
+    handleArrow(information, informationAccordion);
+  });
 }
