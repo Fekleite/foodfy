52bb10eda75164bd62bf1ecab708ed07876d29d9
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-11 22:41:35 -0300

    feat: create page to register recipes

--- routes.js
+++ routes.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 
 const routes = express.Router();
 
-const recipes = require("./data");
+const data = require("./data.json");
 
 // User routes
 
 routes.get("/", (req, res) => {
-  return res.render("user/home", { recipes });
+  return res.render("user/home", { recipes: data.recipes });
 });
 
 routes.get("/about", (req, res) => {
@@ -16,12 +16,12 @@ routes.get("/about", (req, res) => {
 });
 
 routes.get("/recipes", (req, res) => {
-  return res.render("user/recipes", { recipes });
+  return res.render("user/recipes", { recipes: data.recipes });
 });
 
 routes.get("/recipes/:index", (req, res) => {
   const recipeIndex = req.params.index;
-  const recipe = recipes[recipeIndex];
+  const recipe = data.recipes[recipeIndex];
 
   return res.render("user/detail", { recipe });
 });
@@ -29,7 +29,35 @@ routes.get("/recipes/:index", (req, res) => {
 // Admin routes
 
 routes.get("/admin/recipes", (req, res) => {
-  return res.render("admin/index", { recipes });
+  return res.render("admin/index", { recipes: data.recipes });
 });
+
+routes.get("/admin/recipes/create", (req, res) => {
+  return res.render("admin/create");
+});
+
+routes.post("/admin/recipes", (req, res) => {
+  const keys = Object.keys(req.body);
+
+  let { image, title, author, ingredients, preparation, information } = req.body;
+
+  keys.map((key) => {
+    if (req.body[key] === "") {
+      return res.send("Please, fill all fields!");
+    }
+  });
+
+  id = Number(data.recipes.length + 1);
+
+  data.recipes.push({ id, image, title, author, ingredients, preparation, information});
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Write file error!");
+
+    return res.redirect("/admin/recipes");
+  });
+});
+
+
 
 module.exports = routes;
