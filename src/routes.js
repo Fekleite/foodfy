641a2d358441612ae3fbde9f169f4ff12e5c792c
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-13 15:26:16 -0300

    feat: create admin controller

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const GlobalController = require("./app/controllers/GlobalController");
 const RecipeController = require("./app/controllers/RecipeController");
+const AdminController = require("./app/controllers/AdminController");
+
+const multer = require("./app/middlewares/multer");
 
 const routes = express.Router();
 
@@ -11,92 +14,13 @@ routes.get("/about", GlobalController.about);
 routes.get("/recipes", RecipeController.index);
 routes.get("/recipes/:id", RecipeController.show);
 
-// Admin routes
-
-routes.get("/admin/recipes", (req, res) => {
-  return res.render("admin/index", { recipes: data.recipes });
-});
-
-routes.get("/admin/recipes/create", (req, res) => {
-  return res.render("admin/create");
-});
-
-routes.post("/admin/recipes", (req, res) => {
-  const keys = Object.keys(req.body);
-
-  let { image, title, author, ingredients, preparation, information } = req.body;
-
-  keys.map((key) => {
-    if (req.body[key] === "") {
-      return res.send("Please, fill all fields!");
-    }
-  });
-
-  id = Number(data.recipes.length + 1);
-
-  data.recipes.push({ id, image, title, author, ingredients, preparation, information});
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect("/admin/recipes");
-  });
-});
-
-routes.get("/admin/recipes/:id", (req, res) => {
-  const recipeId = Number(req.params.id);
-
-  const foundRecipe = data.recipes.filter((recipe) => recipe.id === recipeId);
-
-  if (!foundRecipe || foundRecipe.length === 0) return res.send("Recipe not found!");
-
-  return res.render("admin/show", { recipe: foundRecipe[0] });
-});
-
-routes.get("/admin/recipes/:id/edit", (req, res) => {
-  const recipeId = Number(req.params.id);
-
-  const foundRecipe = data.recipes.filter((recipe) => recipe.id === recipeId);
-
-  if (!foundRecipe || foundRecipe.length === 0) return res.send("Recipe not found!");
-
-  return res.render("admin/edit", { recipe: foundRecipe[0] });
-});
-
-routes.put("/admin/recipes", (req, res) => {
-  const { id } = req.body;
-  const numberId = Number(id);
-
-  const foundRecipe = data.recipes.filter((recipe) => recipe.id === numberId);
-
-  if (!foundRecipe || foundRecipe.length === 0) return res.send("Recipe not found!");
-
-  const recipe = { ...req.body }
-
-  data.recipes[numberId - 1] = recipe;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect("/admin/recipes");
-  });
-});
-
-routes.delete("/admin/recipes", (req, res) => {
-  const { id } = req.body;
-  const numberId = Number(id);
-
-  const filteredRecipes = data.recipes.filter((recipe) => recipe.id !== numberId );
-
-  data.recipes = filteredRecipes;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect("/admin/recipes");
-  });
-});
-
+routes.get("/admin/recipes", AdminController.index);
+routes.get("/admin/recipes/create", AdminController.create);
+routes.post("/admin/recipes", multer.array("photos", 5), AdminController.post);
+routes.get("/admin/recipes/:id", AdminController.show);
+routes.get("/admin/recipes/:id/edit", AdminController.edit);
+routes.put("/admin/recipes", multer.array("photos", 5), AdminController.put);
+routes.delete("/admin/recipes", AdminController.delete);
 
 
 module.exports = routes;
