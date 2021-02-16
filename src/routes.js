ef0e05b221ee2aec13c64ec4a7e3efd050622940
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-16 15:43:08 -0300

    feat: create searchbar and results page

--- src/routes.js
+++ src/routes.js
@@ -16,22 +16,26 @@ routes.get("/about", GlobalController.about);
 routes.get("/recipes", RecipeController.index);
 routes.get("/recipes/:id", RecipeController.show);
 
+routes.get("/chefs", ChefController.index);
+
 routes.get("/admin/recipes", RecipeAdminController.index);
 routes.get("/admin/recipes/create", RecipeAdminController.create);
-routes.post("/admin/recipes", multer.array("photos", 5), RecipeAdminController.post);
 routes.get("/admin/recipes/:id", RecipeAdminController.show);
 routes.get("/admin/recipes/:id/edit", RecipeAdminController.edit);
+
+routes.post("/admin/recipes", multer.array("photos", 5), RecipeAdminController.post);
 routes.put("/admin/recipes", multer.array("photos", 5), RecipeAdminController.put);
 routes.delete("/admin/recipes", RecipeAdminController.delete);
 
-routes.get("/chefs", ChefController.index);
-
 routes.get("/admin/chefs", ChefAdminController.index);
 routes.get("/admin/chefs/create", ChefAdminController.create);
-routes.post("/admin/chefs", multer.array("photos", 1), ChefAdminController.post);
 routes.get("/admin/chefs/:id", ChefAdminController.show);
 routes.get("/admin/chefs/:id/edit", ChefAdminController.edit);
+
+routes.post("/admin/chefs", multer.array("photos", 1), ChefAdminController.post);
 routes.put("/admin/chefs", multer.array("photos", 1), ChefAdminController.put);
 routes.delete("/admin/chefs", ChefAdminController.delete);
+
+routes.get("/results", SearchController.index);
 
 module.exports = routes;
